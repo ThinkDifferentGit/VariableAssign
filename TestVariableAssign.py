67256b6a50dea16255209a70a59eeb307bb07a4b
Brandon Marroquin <brandon@Brandon-Mac.local>
2025-04-08 14:05:51 -0700

    Initial commit

---
+++ TestVariableAssign.py
@@ -0,0 +1,29 @@
+x, y, z = "Orange", "Banana", "Cherry"
+andon@Brandon-Mac VariableAssign % x, y, z = "Orange", "Banana", "Cherry"
+zsh: command not found: x,
+brandon@Brandon-Mac VariableAssign % x, y, z = "Orange", "Banana", "Cherry"
+zsh: command not found: x,
+brandon@Brandon-Mac VariableAssign %
+brandon@Brandon-Mac VariableAssign %
+brandon@Brandon-Mac VariableAssign % x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+zsh: command not found: x,
+zsh: no matches found: print(x)
+brandon@Brandon-Mac VariableAssign % x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+zsh: command not found: x
+zsh: no matches found: print(x)
+brandon@Brandon-Mac VariableAssign % fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+zsh: bad pattern: [apple,
+zsh: command not found: x,
+zsh: no matches found: print(x)
+brandon@Brandon-Mac VariableAssign %
